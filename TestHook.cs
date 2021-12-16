7113f5934dc69371e01ad3c49021a9ce832cd0de
John Carew <johnwc@users.noreply.github.com>
2021-12-15 22:54:22 -0600

    Add project files.

---
+++ TestHook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class TestHook
+    {
+        [UnmanagedCallersOnly(EntryPoint = "FancyName")]
+        [DNNE.C99DeclCode("#include <hooks/hooks.h>")]
+        public static int MyExport(int a)
+        {
+            return a;
+        }
+    }
+}
